df6131e27331c5be66c6a683945d25b3d7a36293
윤다예 <daye.elin.yoon@gmail.com>
2025-01-22 15:24:01 +0900

    chore: core module scaffold 추가

---
+++ Tuist/Templates/Core/Core.swift
@@ -0,0 +1,57 @@
+//
+//  Example.swift
+//  Manifests
+//
+//  Created by 김진우 on 1/16/25.
+//
+
+import ProjectDescription
+
+let name = Template.Attribute.required("name")
+
+let template = Template(
+    description: "Core Scaffold",
+    attributes: [
+        name,
+    ],
+    items: CoreTemplate.allCases.flatMap{$0.item}
+)
+
+enum CoreTemplate: CaseIterable {
+    case project
+    case source
+    case test
+    
+    var item: [Template.Item] {
+        switch self {
+        case .project:
+            return [.file(
+                path: .basePath + "/Project.swift",
+                templatePath: "Project.stencil"
+            )]
+
+        case .source:
+            return [
+                .file(
+                    path: .basePath + "/Sources/\(name)ViewController.swift",
+                    templatePath: "../Core.stencil"
+                )
+            ]
+
+        case .test:
+            return [
+                .file(
+                    path: .basePath + "/Tests/\(name)Tests.swift",
+                    templatePath: "../Test.stencil"
+                )
+            ]
+        }
+        
+    }
+}
+
+extension String {
+    static var basePath: Self {
+        return "Projects/Core/CokeZet-\(name)"
+    }
+}
