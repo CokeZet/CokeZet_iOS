f1ea175a705b5c3646f8018a9801f4254ea024ee
윤다예 <daye.elin.yoon@gmail.com>
2025-01-24 12:50:59 +0900

    feat: nickname inputView init

---
+++ Projects/Features/Nickname-Feature/Sources/Components/NicknameInputView.swift
@@ -0,0 +1,22 @@
+//
+//  NicknameInputView.swift
+//  Nickname-Feature
+//
+//  Created by Daye on 1/24/25.
+//
+
+import UIKit
+
+import SnapKit
+
+internal final class NicknameInputView: UIView {
+
+    internal override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
