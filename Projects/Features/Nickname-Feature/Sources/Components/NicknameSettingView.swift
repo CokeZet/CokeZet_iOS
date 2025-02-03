8b0d3178f982728ea79eb5e17f40e2ce24519ad7
윤다예 <daye.elin.yoon@gmail.com>
2025-02-03 16:46:26 +0900

    temp

---
+++ Projects/Features/Nickname-Feature/Sources/Components/NicknameSettingView.swift
@@ -0,0 +1,17 @@
+//
+//  NicknameSettingView.swift
+//  Nickname-Feature
+//
+//  Created by Daye on 1/24/25.
+//
+
+import UIKit
+
+import CokeZet_DesignSystem
+
+import SnapKit
+
+internal final class NicknameSettingView: UIView {
+
+    private let inputView = NicknameInputView(placeHolder: <#T##String#>)
+}
