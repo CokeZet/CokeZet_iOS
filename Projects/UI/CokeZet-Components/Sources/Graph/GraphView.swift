70f560ee798f784586948b71d195c4c1e2c72c0b
김진우 <osc2143@gmail.com>
2025-02-06 11:32:13 +0900

    feat: graphView UI 작업 완료

--- Projects/UI/CokeZet-Components/Sources/Graph/GraphView.swift
+++ Projects/UI/CokeZet-Components/Sources/Graph/GraphView.swift
@@ -26,6 +26,12 @@ public final class GraphView: UICollectionViewCell {
 
     // 그래프를 그릴 뷰
     private let graphView = UIView()
+    
+    private let titleLabel: UILabel = UILabel().then {
+        $0.text = "최저가 그래프"
+        $0.font = Typography.T18.font
+        $0.textColor = .White
+    }
 
     private var isGraphDrawn = false // 그래프가 이미 그려졌는지 확인하는 플래그
 
@@ -43,44 +49,64 @@ public final class GraphView: UICollectionViewCell {
         // 상단 버튼 컨테이너
         let buttonStackView = UIStackView()
         buttonStackView.axis = .horizontal
-        buttonStackView.distribution = .fillEqually
+        buttonStackView.distribution = .equalSpacing
+        buttonStackView.alignment = .center
         buttonStackView.spacing = 8
-
+        buttonStackView.layer.backgroundColor = UIColor.Gray900.cgColor
+        buttonStackView.layer.cornerRadius = 24
+        
+        let emptyView = UIView()
+        buttonStackView.addArrangedSubview(emptyView)
         // 버튼 추가
         let periods = ["1개월", "3개월", "6개월"]
         for (index, period) in periods.enumerated() {
             let button = UIButton(type: .system)
             button.setTitle(period, for: .normal)
-            button.setTitleColor(index == 0 ? .white : .gray, for: .normal)
-            button.backgroundColor = index == 0 ? .darkGray : .clear
-            button.layer.cornerRadius = 8
+            button.setTitleColor(index == 0 ? .White : .Gray600, for: .normal)
+            button.backgroundColor = index == 0 ? .Gray700 : .clear
+            button.layer.cornerRadius = 16
             button.tag = index
             button.addTarget(self, action: #selector(periodButtonTapped(_:)), for: .touchUpInside)
+            button.snp.makeConstraints {
+                $0.height.equalTo(32)
+                $0.width.equalTo((self.bounds.width - 40 - (8 * 4)) / 3 )
+            }
             buttonStackView.addArrangedSubview(button)
         }
-
+        let emptyView2 = UIView()
+        buttonStackView.addArrangedSubview(emptyView2)
+        
+        [titleLabel, buttonStackView].forEach {
+            addSubview($0)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(28)
+            $0.leading.equalToSuperview().offset(20)
+            $0.trailing.equalToSuperview().offset(-20)
+        }
+        
         // 버튼 스택뷰 추가 및 레이아웃 설정
-        addSubview(buttonStackView)
         buttonStackView.snp.makeConstraints { make in
-            make.top.equalTo(safeAreaLayoutGuide).offset(16)
-            make.leading.equalToSuperview().offset(16)
-            make.trailing.equalToSuperview().offset(-16)
-            make.height.equalTo(40)
+            make.top.equalTo(titleLabel.snp.bottom).offset(24)
+            make.leading.equalToSuperview().offset(20)
+            make.trailing.equalToSuperview().offset(-20)
+            make.height.equalTo(48)
         }
 
         // 그래프 뷰 추가 및 레이아웃 설정
         addSubview(graphView)
         graphView.snp.makeConstraints { make in
-            make.top.equalTo(buttonStackView.snp.bottom).offset(32)
-            make.leading.equalToSuperview().offset(16)
-            make.trailing.equalToSuperview().offset(-16)
+            make.top.equalTo(buttonStackView.snp.bottom).offset(24)
+            make.leading.equalToSuperview().offset(55)
+            make.trailing.equalToSuperview().offset(-55)
             make.height.equalTo(200)
         }
     }
 
     public override func layoutSubviews() {
         super.layoutSubviews()
-
+        
         // 레이아웃이 완료된 후에 한 번만 그래프를 그림
         if !isGraphDrawn {
             drawGraph()
@@ -95,7 +121,7 @@ public final class GraphView: UICollectionViewCell {
 
         // 그래프 경계
         let graphWidth = graphView.bounds.width
-        let graphHeight = graphView.bounds.height
+        let graphHeight = graphView.bounds.height - 100
 
         // 가격 범위 계산
         guard let minPrice = priceData.map({ $0.price }).min(),
@@ -123,7 +149,7 @@ public final class GraphView: UICollectionViewCell {
 
             // 최저가 점을 하단에서 50px 위로 이동
             if data.price == minPrice {
-                yPosition = graphHeight - 40
+                yPosition = graphHeight
                 minPricePoint = CGPoint(x: xPosition, y: yPosition)
             }
 
@@ -137,7 +163,7 @@ public final class GraphView: UICollectionViewCell {
             // 데이터 점 추가 (원)
             let circle = UIView()
             circle.backgroundColor = .black // 원 내부 색상 설정
-            circle.layer.borderColor = UIColor.blue.cgColor
+            circle.layer.borderColor = UIColor.Purple500.cgColor
             circle.layer.borderWidth = 1
             circle.layer.cornerRadius = 4
             graphView.addSubview(circle)
@@ -146,29 +172,34 @@ public final class GraphView: UICollectionViewCell {
                 make.centerY.equalTo(yPosition)
                 make.width.height.equalTo(8)
             }
+            
+            // 날짜 레이블 추가
+            let dateLabel = UILabel()
+            dateLabel.text = data.date
+            dateLabel.font = Typography.T12.font
+            dateLabel.textColor = .Gray500
+//            dateLabel.sizeToFit()
+            dateLabel.layer.cornerRadius = 4
+            dateLabel.layer.backgroundColor = UIColor.Gray700.cgColor
+            dateLabel.textAlignment = .center
+            graphView.addSubview(dateLabel)
+            dateLabel.snp.makeConstraints { make in
+                make.centerX.equalTo(xPosition)
+                make.width.equalTo(55)
+                make.height.equalTo(20)
+                make.bottom.equalTo(graphView.snp.bottom).inset(10)
+            }
 
             // 가격 레이블 추가
             let priceLabel = UILabel()
-            priceLabel.text = "\(data.price)원"
-            priceLabel.font = .systemFont(ofSize: 12)
-            priceLabel.textColor = .white
+            priceLabel.text = String(data.price).formatWithComma() + "원"
+            priceLabel.font = Typography.T14.font
+            priceLabel.textColor = data.price == minPrice ? .Purple500 : .Gray300
             priceLabel.sizeToFit()
             graphView.addSubview(priceLabel)
             priceLabel.snp.makeConstraints { make in
                 make.centerX.equalTo(xPosition)
-                make.bottom.equalTo(circle.snp.top).offset(-8)
-            }
-
-            // 날짜 레이블 추가
-            let dateLabel = UILabel()
-            dateLabel.text = data.date
-            dateLabel.font = .systemFont(ofSize: 10)
-            dateLabel.textColor = .gray
-            dateLabel.sizeToFit()
-            graphView.addSubview(dateLabel)
-            dateLabel.snp.makeConstraints { make in
-                make.centerX.equalTo(xPosition)
-                make.top.equalTo(graphView.snp.bottom).offset(10)
+                make.bottom.equalTo(dateLabel.snp.top).offset(-8)
             }
         }
 
@@ -176,11 +207,11 @@ public final class GraphView: UICollectionViewCell {
         if let minPricePoint = minPricePoint {
             let verticalLinePath = UIBezierPath()
             verticalLinePath.move(to: minPricePoint)
-            verticalLinePath.addLine(to: CGPoint(x: minPricePoint.x, y: graphHeight))
+            verticalLinePath.addLine(to: CGPoint(x: minPricePoint.x, y: graphHeight + 35))
 
             let verticalLineLayer = CAShapeLayer()
             verticalLineLayer.path = verticalLinePath.cgPath
-            verticalLineLayer.strokeColor = UIColor.red.cgColor
+            verticalLineLayer.strokeColor = UIColor.Purple500.cgColor
             verticalLineLayer.lineWidth = 1
             verticalLineLayer.lineDashPattern = [2, 2] // 점선 스타일
 
@@ -190,7 +221,7 @@ public final class GraphView: UICollectionViewCell {
         // 그래프 선을 그리는 레이어 추가
         let shapeLayer = CAShapeLayer()
         shapeLayer.path = path.cgPath
-        shapeLayer.strokeColor = UIColor.blue.cgColor
+        shapeLayer.strokeColor = UIColor.Purple500.cgColor
         shapeLayer.lineWidth = 2
         shapeLayer.fillColor = UIColor.clear.cgColor
 
@@ -201,8 +232,8 @@ public final class GraphView: UICollectionViewCell {
     @objc private func periodButtonTapped(_ sender: UIButton) {
         // 버튼 스타일 업데이트
         for case let button as UIButton in sender.superview?.subviews ?? [] {
-            button.setTitleColor(button == sender ? .white : .gray, for: .normal)
-            button.backgroundColor = button == sender ? .darkGray : .clear
+            button.setTitleColor(button == sender ? .White : .Gray600, for: .normal)
+            button.backgroundColor = button == sender ? .Gray700 : .clear
         }
 
         // 데이터 업데이트 (예시: 버튼에 따라 데이터 변경 가능)
