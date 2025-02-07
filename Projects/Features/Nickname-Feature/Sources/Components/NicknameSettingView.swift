45d816d0e48715a0bcf4b6e0170eda86cfe91b90
윤다예 <daye.elin.yoon@gmail.com>
2025-02-07 16:55:18 +0900

    feat: nicknameSettingView 레이아웃 구성

--- Projects/Features/Nickname-Feature/Sources/Components/NicknameSettingView.swift
+++ Projects/Features/Nickname-Feature/Sources/Components/NicknameSettingView.swift
@@ -13,5 +13,69 @@ import SnapKit
 
 internal final class NicknameSettingView: UIView {
 
-    private let inputView = NicknameInputView(placeHolder: <#T##String#>)
+    private let stackView = UIStackView()
+    private let nickNameinputView = NicknameInputView()
+    private let descriptionLabel = ZetLabel(typography: .T24, textColor: .White)
+    private let confirmButton = ZetRoundButton(buttonState: .Disabled)
+
+    internal init() {
+        super.init(frame: .zero)
+        self.addConfigure()
+        self.makeConstraints()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func addConfigure() {
+        self.backgroundColor = .Gray800
+
+        self.stackView.axis = .vertical
+        self.stackView.spacing = 24
+        self.stackView.alignment = .leading
+
+        self.descriptionLabel.numberOfLines = 2
+        self.descriptionLabel.text = "몇 가지 더 설정하고\n함께 최저가 콜라 즐겨요!"
+        self.descriptionLabel.textAlignment = .left
+        self.confirmButton.setTitle("다음", for: .normal)
+    }
+
+    private func makeConstraints() {
+        self.addSubview(stackView)
+        self.stackView.addArrangedSubview(nickNameinputView)
+        self.stackView.addArrangedSubview(descriptionLabel)
+
+        self.addSubview(confirmButton)
+
+        self.stackView.snp.makeConstraints {
+            $0.top.equalTo(self.safeAreaLayoutGuide).offset(88)
+            $0.leading.equalToSuperview().offset(20)
+        }
+
+        self.confirmButton.snp.makeConstraints {
+            $0.bottom.equalTo(self.safeAreaLayoutGuide).offset(-17)
+            $0.centerX.equalToSuperview()
+        }
+    }
+
 }
+
+@available(iOS 17.0, *)
+#Preview(
+    "normal",
+    traits: .sizeThatFitsLayout
+) {
+    let contentView = UIView()
+
+    let view = NicknameSettingView()
+    contentView.addSubview(view)
+    view.snp.makeConstraints {
+        $0.edges.equalToSuperview()
+    }
+
+    return contentView
+}
+
+
