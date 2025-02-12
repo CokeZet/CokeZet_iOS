9c4648b015ad86b8bdd944024067d0bdc78bc0bb
윤다예 <daye.elin.yoon@gmail.com>
2025-02-12 12:12:13 +0900

    feat: chipview init

---
+++ Projects/UI/CokeZet-DesignSystem/Sources/Chip/ChipView.swift
@@ -0,0 +1,32 @@
+//
+//  ChipView.swift
+//  CokeZet-DesignSystem
+//
+//  Created by Daye on 2/12/25.
+//
+
+import UIKit
+
+import SnapKit
+
+final class ChipView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.addConfigure()
+        self.makeConstraints()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func addConfigure() {
+
+    }
+
+    private func makeConstraints() {
+
+    }
+}
