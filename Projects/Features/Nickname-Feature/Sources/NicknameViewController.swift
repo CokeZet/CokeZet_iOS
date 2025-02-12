86099ccf38bda55738db190f96a39c0eb79774d7
윤다예 <daye.elin.yoon@gmail.com>
2025-02-12 11:51:44 +0900

    feat: view-vc 연결

--- Projects/Features/Nickname-Feature/Sources/NicknameViewController.swift
+++ Projects/Features/Nickname-Feature/Sources/NicknameViewController.swift
@@ -12,29 +12,29 @@ protocol NicknamePresentableListener: AnyObject {
 
 final class NicknameViewController: UIViewController, NicknamePresentable, NicknameViewControllable {
 
-    let label: UILabel = UILabel().then {
-        $0.text = "안녕하세요 Nickname 입니다."
-    }
-    
+    let contentView = NicknameSettingView()
+
     weak var listener: NicknamePresentableListener?
-    
+
     init() {
         super.init(nibName: nil, bundle: nil)
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
+    override func loadView() {
+        self.view = contentView
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
         setupViews()
     }
-    
+
     func setupViews() {
-        self.view.addSubview(label)
-        label.snp.makeConstraints {
-            $0.centerX.centerY.equalToSuperview()
-        }
-    }}
+
+    }
+}
