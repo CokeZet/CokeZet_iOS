018e2ce9d474206ad1e643de7c36348a8f9f33d3
윤다예 <daye.elin.yoon@gmail.com>
2025-02-14 09:43:03 +0900

    feat: 레이아웃 작성

--- Projects/UI/CokeZet-DesignSystem/Sources/Badge/BadgeMarketView.swift
+++ Projects/UI/CokeZet-DesignSystem/Sources/Badge/BadgeMarketView.swift
@@ -11,16 +11,44 @@ import SnapKit
 
 public final class BadgeMarketView: UIView {
 
-    
+    private enum Metric {
+        static let size: CGSize = CGSize(width: 34, height: 40)
+    }
+
+    private let imageView = UIImageView()
 
     public init() {
         super.init(frame: .zero)
-        
-        backgroundColor = .systemBlue
-        layer.cornerRadius = 8
+        self.makeConstraints()
     }
-    
+
+    @available(*, unavailable)
     public required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+
+    private func makeConstraints() {
+        self.addSubview(imageView)
+
+        self.imageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+            $0.size.equalTo(Metric.size)
+        }
+    }
+
+    public func setImage(_ image: UIImage) {
+        self.imageView.image = image
+    }
+}
+
+@available(iOS 17.0, *)
+#Preview(
+    "normal",
+    traits: .sizeThatFitsLayout
+) {
+    let view = BadgeMarketView()
+
+    view.setImage(CokeZetDesignSystemAsset.icMarketNaver.image)
+
+    return view
 }
