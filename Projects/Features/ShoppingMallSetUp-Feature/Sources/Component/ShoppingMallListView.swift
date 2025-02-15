1503daa3d6cb5d54c1910a81cac55bfa84958835
윤다예 <daye.elin.yoon@gmail.com>
2025-02-15 18:03:43 +0900

    feat: listView init

---
+++ Projects/Features/ShoppingMallSetUp-Feature/Sources/Component/ShoppingMallListView.swift
@@ -0,0 +1,32 @@
+//
+//  ShoppingMallListView.swift
+//  ShoppingMallSetUp-Feature
+//
+//  Created by Daye on 2/15/25.
+//
+
+import UIKit
+
+import SnapKit
+
+final class ShoppingMallListView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.addConfigure()
+        self.makeConstraints()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func addConfigure() {
+
+    }
+
+    private func makeConstraints() {
+
+    }
+}
