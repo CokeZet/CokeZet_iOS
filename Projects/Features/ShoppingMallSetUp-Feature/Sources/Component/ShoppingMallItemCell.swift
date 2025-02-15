3f92a7f496884b65940d0a29db8fbdbfcbbcf201
윤다예 <daye.elin.yoon@gmail.com>
2025-02-15 17:54:22 +0900

    feat: ShoppingMallItemCell init

---
+++ Projects/Features/ShoppingMallSetUp-Feature/Sources/Component/ShoppingMallItemCell.swift
@@ -0,0 +1,32 @@
+//
+//  ShoppingMallItemCell.swift
+//  ShoppingMallSetUp-Feature
+//
+//  Created by Daye on 2/15/25.
+//
+
+import UIKit
+
+import SnapKit
+
+final class ShoppingMallItemCell: UICollectionViewCell {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.addConfigure()
+        self.makeConstraints()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func addConfigure() {
+
+    }
+
+    private func makeConstraints() {
+
+    }
+}
