c5ed3649a0e71e7073fa130950640a8677d677f7
윤다예 <daye.elin.yoon@gmail.com>
2025-02-15 18:02:13 +0900

    feat: 레이아웃 작성

--- Projects/Features/ShoppingMallSetUp-Feature/Sources/Component/ShoppingMallItemCell.swift
+++ Projects/Features/ShoppingMallSetUp-Feature/Sources/Component/ShoppingMallItemCell.swift
@@ -7,9 +7,24 @@
 
 import UIKit
 
+import CokeZet_DesignSystem
+
 import SnapKit
 
 final class ShoppingMallItemCell: UICollectionViewCell {
+
+    private enum Metric {
+        static let spacing: CGFloat = 8
+    }
+
+    struct State {
+        let image: UIImage
+        let title: String
+    }
+
+    private let stackView = UIStackView()
+    private let choiceView = ChoiceView()
+    private let titleLabel = ZetLabel(typography: .semiBold(.T16))
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -23,10 +38,36 @@ final class ShoppingMallItemCell: UICollectionViewCell {
     }
 
     private func addConfigure() {
+        self.stackView.axis = .vertical
+        self.stackView.spacing = Metric.spacing
 
+        self.titleLabel.textColor = .Gray500
     }
 
     private func makeConstraints() {
+        self.addSubview(stackView)
 
+        self.stackView.addArrangedSubview(choiceView)
+        self.stackView.addArrangedSubview(titleLabel)
+
+        self.stackView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+
+    func bind(state: State) {
+        self.choiceView.setImage(state.image)
+        self.titleLabel.text = state.title
+    }
+
+    func setChoiceState(_ state: ChoiceState) {
+        self.choiceView.setState(state)
+
+        switch state {
+        case .active:
+            self.titleLabel.textColor = .White
+        case .normal:
+            self.titleLabel.textColor = .Gray500
+        }
     }
 }
