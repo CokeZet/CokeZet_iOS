57ad18a670703538cf8ed6d800f9199878ef26d7
윤다예 <daye.elin.yoon@gmail.com>
2025-02-16 16:15:10 +0900

    feat: benefit view init

---
+++ Projects/Features/Main-Feature/Sources/Components/CardBenefitView.swift
@@ -0,0 +1,34 @@
+//
+//  CardBenefitView.swift
+//  Main-Feature
+//
+//  Created by Daye on 2/16/25.
+//
+
+import UIKit
+
+import CokeZet_DesignSystem
+
+import SnapKit
+
+final class CardBenefitView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.addConfigure()
+        self.makeConstraints()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func addConfigure() {
+
+    }
+
+    private func makeConstraints() {
+
+    }
+}
