801f36b0f4537ca3b9be187d2d65420987693e7c
윤다예 <daye.elin.yoon@gmail.com>
2025-02-16 16:25:44 +0900

    feat: benefit ui 작성

--- Projects/Features/Main-Feature/Sources/Components/CardBenefitView.swift
+++ Projects/Features/Main-Feature/Sources/Components/CardBenefitView.swift
@@ -12,7 +12,22 @@ import CokeZet_DesignSystem
 import SnapKit
 
 final class CardBenefitView: UIView {
-    
+
+    private enum Metric {
+        static let spacing: CGFloat = 16
+        static let badgeSpacing: CGFloat = 6
+        static let badgeSize: CGFloat = 20
+    }
+
+    struct State {
+        let list: [UIImage]
+    }
+
+    private let stackView = UIStackView()
+
+    private let titleLabel = ZetLabel(typography: .semiBold(.T14), textColor: .Gray500)
+    private let badgeStackView = UIStackView()
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.addConfigure()
@@ -25,10 +40,60 @@ final class CardBenefitView: UIView {
     }
 
     private func addConfigure() {
+        self.stackView.axis = .horizontal
+        self.stackView.spacing = Metric.spacing
 
+        self.titleLabel.text = "카드혜택"
+
+        self.badgeStackView.axis = .horizontal
+        self.badgeStackView.spacing = Metric.badgeSpacing
     }
 
     private func makeConstraints() {
+        self.addSubview(stackView)
+        self.stackView.addArrangedSubview(titleLabel)
+        self.stackView.addArrangedSubview(badgeStackView)
 
+        self.stackView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
     }
+
+    func bind(state: State) {
+        state.list.forEach {
+            let badgeView = UIImageView() // 임시
+            badgeView.image = $0
+
+            badgeView.snp.makeConstraints {
+                $0.size.equalTo(Metric.badgeSize)
+            }
+
+            self.badgeStackView.addArrangedSubview(badgeView)
+        }
+    }
+}
+
+@available(iOS 17.0, *)
+#Preview(
+    "normal",
+    traits: .sizeThatFitsLayout
+) {
+    let contentView = UIView()
+    contentView.backgroundColor = .Gray700
+
+    let view = CardBenefitView()
+    contentView.addSubview(view)
+    view.snp.makeConstraints {
+        $0.edges.equalToSuperview()
+    }
+
+    view.bind(state: CardBenefitView.State(list: [
+        CokeZetDesignSystemAsset.icShinhan.image,
+        CokeZetDesignSystemAsset.icKb.image,
+        CokeZetDesignSystemAsset.icLotteCard.image,
+        CokeZetDesignSystemAsset.icHyundai.image,
+        CokeZetDesignSystemAsset.icSamsung.image,
+    ]))
+
+    return contentView
 }
