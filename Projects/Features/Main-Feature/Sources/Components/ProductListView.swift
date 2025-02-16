c02438b580f91b33783ea8151677e520dec14b11
윤다예 <daye.elin.yoon@gmail.com>
2025-02-16 16:51:41 +0900

    feat: productListView init

---
+++ Projects/Features/Main-Feature/Sources/Components/ProductListView.swift
@@ -0,0 +1,35 @@
+//
+//  ProductListHeaderView.swift
+//  Main-Feature
+//
+//  Created by Daye on 2/16/25.
+//
+
+import UIKit
+
+import CokeZet_DesignSystem
+
+import SnapKit
+
+final class ProductListView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.addConfigure()
+        self.makeConstraints()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func addConfigure() {
+
+    }
+
+    private func makeConstraints() {
+
+    }
+
+}
