4deaf5f5233098373d91c9f37296142c272bc871
윤다예 <daye.elin.yoon@gmail.com>
2025-02-18 13:50:58 +0900

    feat: banner item cell init

---
+++ Projects/Features/Main-Feature/Sources/Components/BannerItemCell.swift
@@ -0,0 +1,35 @@
+//
+//  BannerItemCell.swift
+//  Main-Feature
+//
+//  Created by Daye on 2/18/25.
+//
+
+import UIKit
+
+import CokeZet_DesignSystem
+
+import SnapKit
+
+final class BannerItemCell: UICollectionViewCell {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.addConfigure()
+        self.makeConstraints()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func addConfigure() {
+
+    }
+
+    private func makeConstraints() {
+
+    }
+    
+}
