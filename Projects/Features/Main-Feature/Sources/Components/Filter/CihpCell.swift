2f6369ce3d499075df7c4f09a0bbf02a9ae2e8d5
윤다예 <daye.elin.yoon@gmail.com>
2025-02-25 16:34:52 +0900

    feat: chip cell init

---
+++ Projects/Features/Main-Feature/Sources/Components/Filter/CihpCell.swift
@@ -0,0 +1,22 @@
+//
+//  CihpCell.swift
+//  Main-Feature
+//
+//  Created by Daye on 2/25/25.
+//
+
+import UIKit
+
+import SnapKit
+
+final class CihpCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
