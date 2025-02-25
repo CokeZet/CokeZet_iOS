71425796910b900139dbcb2299e73949b081dff6
윤다예 <daye.elin.yoon@gmail.com>
2025-02-25 16:42:12 +0900

    feat: 레이아웃 작성

--- Projects/Features/Main-Feature/Sources/Components/Filter/CihpCell.swift
+++ Projects/Features/Main-Feature/Sources/Components/Filter/CihpCell.swift
@@ -7,16 +7,29 @@
 
 import UIKit
 
+import CokeZet_DesignSystem
+
 import SnapKit
 
 final class CihpCell: UICollectionViewCell {
-    
+
+    private let chipView = FilterView()
+
     override init(frame: CGRect) {
         super.init(frame: frame)
+        self.makeConstraints()
     }
 
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    private func makeConstraints() {
+        self.contentView.addSubview(chipView)
+
+        self.chipView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
     }
 }
