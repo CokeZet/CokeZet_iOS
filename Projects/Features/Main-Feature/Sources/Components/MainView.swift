1f8ad3abd2bc9857308e57d8ede4aa7bac46f8b7
윤다예 <daye.elin.yoon@gmail.com>
2025-02-27 14:33:02 +0900

    feat: mainView init

---
+++ Projects/Features/Main-Feature/Sources/Components/MainView.swift
@@ -0,0 +1,33 @@
+//
+//  MainView.swift
+//  Main-Feature
+//
+//  Created by Daye on 2/27/25.
+//
+
+import UIKit
+
+import SnapKit
+
+final class MainView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.addConfigure()
+        self.makeConstraints()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func addConfigure() {
+
+    }
+
+    private func makeConstraints() {
+        
+    }
+
+}
