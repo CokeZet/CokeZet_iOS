890069b68d0d1a25bfee4f6d7db85a49ddf9f53d
윤다예 <daye.elin.yoon@gmail.com>
2025-03-10 10:00:50 +0900

    feat: module init

---
+++ Projects/Features/Login-Feature/Sources/LoginBuilder.swift
@@ -0,0 +1,43 @@
+//
+//  Builder.swift
+//  Manifests
+//
+//  Created by 김진우 on 1/16/25.
+//
+
+import ModernRIBs
+import Features
+
+protocol LoginDependency: Dependency {
+    // TODO: Declare the set of dependencies required by this RIB, but cannot be
+    // created by this RIB.
+}
+
+final class LoginComponent: Component<LoginDependency> {
+    
+    // TODO: Declare 'fileprivate' dependencies that are only used by this RIB.
+}
+
+// MARK: - Builder
+
+protocol LoginBuildable: Buildable {
+    func build() -> LaunchRouting
+}
+
+final class LoginBuilder: Builder<LoginDependency>, LoginBuildable {
+    
+    override init(dependency: LoginDependency) {
+        super.init(dependency: dependency)
+    }
+    
+    public func build() -> LaunchRouting {
+        let component = LoginComponent(dependency: dependency)
+        let viewController = LoginViewController()
+        let interactor = LoginInteractor(presenter: viewController)
+        return LoginRouter(
+            interactor: interactor,
+            viewController: viewController
+        )
+    }
+}
+
---
+++ Projects/Features/Login-Feature/Sources/LoginRouter.swift
@@ -0,0 +1,23 @@
+
+import ModernRIBs
+import Features
+
+protocol LoginInteractable: Interactable {
+    var router: LoginRouting? { get set }
+    var listener: LoginListener? { get set }
+}
+
+protocol LoginViewControllable: ViewControllable {
+    // TODO: Declare methods the router invokes to manipulate the view hierarchy.
+}
+
+final class LoginRouter: LaunchRouter<LoginInteractable, LoginViewControllable>, LoginRouting {
+    // TODO: Constructor inject child builder protocols to allow building children.
+    override init(
+        interactor: LoginInteractable,
+        viewController: LoginViewControllable
+    ) {
+        super.init(interactor: interactor, viewController: viewController)
+        interactor.router = self
+    }
+}
